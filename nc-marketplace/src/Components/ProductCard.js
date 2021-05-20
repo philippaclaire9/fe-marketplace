de6f8bee3812ef50d94ffa59859bb8d5aec48a1e
Philippa Bushell <philippabushell@Philippas-MacBook-Pro.local>
2021-05-20 17:06:46 +0100

    add productCard and productList

--- nc-marketplace/src/Components/ProductCard.js
+++ nc-marketplace/src/Components/ProductCard.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ProductCard = ({products}) => {
-  return (
-      products.map((item) => {
-        return (
-          <li key={item.item_id}>
-            <img
-              className="product-image"
-              src={item.img_url}
-              alt={`a ${item.item_name}`}
-            />
-            <p>{item.item_name}</p>
-          </li>
-        );
-      })
-  );
+const ProductCard = ({ products }) => {
+  return products.map((item) => {
+    return (
+      <li key={item.item_id}>
+        <Link to={`/products/${item.item_id}`}>
+          <img
+            className="product-image"
+            src={item.img_url}
+            alt={`a ${item.item_name}`}
+          />{' '}
+        </Link>
+        <p>{item.item_name}</p>
+      </li>
+    );
+  });
 };
 
 export default ProductCard;
--- nc-marketplace/src/Components/ProductList.js
+++ nc-marketplace/src/Components/ProductList.js
@@ -1,24 +1,45 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import React from "react";
-import ProductCard from "./ProductCard";
-const ProductList = () => {
+import { useEffect, useState } from 'react';
+
+import React from 'react';
+import ProductCard from './ProductCard';
+import { fetchCategories, fetchProducts } from '../api';
+import CategoriesDropdown from './CategoriesDropdown';
+
+const ProductList = ({
+  categories,
+  selectedCat,
+  setSelectedCat,
+  setCategories,
+}) => {
   const [products, setProducts] = useState([]);
+  //const [categories, setCategories] = useState([]);
+  // const [selectedCat, setSelectedCat] = useState('');
 
   useEffect(() => {
     // should extract the api call...
-    axios
-      .get("https://nc-marketplace.herokuapp.com/api/items")
-      .then(({ data: { items } }) => {
-        // console.log(response.data.items);
-        setProducts(items);
-      });
-  }, []);
+
+    fetchProducts(selectedCat).then((products) => {
+      setProducts(products);
+    });
+
+    fetchCategories().then((categoriesFromApi) => {
+      //console.log(categoriesFromApi);
+      setCategories(categoriesFromApi);
+    });
+  }, [selectedCat, setCategories]);
 
   return (
-    <ul className="product-list">
-      <ProductCard products={products} />
-    </ul>
+    <main>
+      <CategoriesDropdown
+        categories={categories}
+        selectedCat={selectedCat}
+        setSelectedCat={setSelectedCat}
+      />
+
+      <ul className="product-list">
+        <ProductCard products={products} />
+      </ul>
+    </main>
   );
 };
 
