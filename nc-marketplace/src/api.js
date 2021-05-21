25fce7fecb970f554e8974a59bd2f283f372a3ab
Philippa Bushell <philippabushell@Philippas-MacBook-Pro.local>
2021-05-21 11:18:04 +0100

    extract axios requests

---
+++ nc-marketplace/src/api.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+export const fetchProducts = (category) => {
+  return axios
+    .get(`https://nc-marketplace.herokuapp.com/api/items`, {
+      params: {
+        category_name: category,
+      },
+    })
+    .then(({ data: { items } }) => {
+      return items;
+    });
+};
+
+export const fetchProduct = (item_id) => {
+  return axios
+    .get(`https://nc-marketplace.herokuapp.com/api/items/${item_id}`)
+    .then((response) => {
+      return response.data.item;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const fetchCategories = () => {
+  return axios
+    .get('https://nc-marketplace.herokuapp.com/api/categories')
+    .then((response) => {
+      return response.data.categories;
+    });
+};
